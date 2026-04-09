2a462a8b2fc70d1dd0e4050bf8ca6abad0231e83
AL Fahim Fuyad <fuad556805@gmail.com>
2026-04-09 12:18:49 +0600

    Remove demo data seeding from build.sh
    Removed demo data seeding from the build script.

--- build.sh
+++ build.sh
@@ -7,7 +7,4 @@ python manage.py collectstatic --noinput
 echo "=== Running migrations ==="
 python manage.py migrate --noinput
 
-echo "=== Seeding demo data ==="
-python manage.py seed_demo
-
 echo "=== Build complete ==="
